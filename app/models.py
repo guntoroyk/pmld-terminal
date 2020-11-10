57bfec501b3a073c4b6d6113b960f133684beee0
didilmfs <didil.mfs@gmail.com>
2020-11-10 15:38:10 +0700

    menambahkan data bus dengan fixtures

--- app/models.py
+++ app/models.py
@@ -14,12 +14,12 @@ class Bus(models.Model):
     nama_po = models.CharField(max_length=20)
     plat_no = models.CharField(max_length=10)
     jenis_trayek = models.CharField(max_length=5)
-    jumlah_kursi = models.PositiveIntegerField
-    asal_tujuan_trayek = models.CharField(max_length=30)
+    jumlah_kursi = models.PositiveIntegerField()
+    asal_tujuan_trayek = models.CharField(max_length=50)
 
 
 class PencatatanBus(models.Model):
-    waktu_datang = models.DateTimeField
+    waktu_datang = models.DateTimeField()
     waktu_berangkat = models.DateTimeField(default=timezone.now)
     jumlah_penumpang_datang = models.PositiveIntegerField()
     penumpang_naik = models.PositiveIntegerField()
---
+++ csvtoyaml.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import csv
+import os
+def from_csv_to_yaml(file):
+    with open(file, 'r') as csvfile:
+        freader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        # Creates the output file named fixture.yaml
+        f = open('fixture.yaml', 'w')
+        for i, row in enumerate(freader):
+            csv_content = ' '.join(row)
+            csv_content = csv_content.split(',')
+            f.write('- model: app.Bus\n')
+            f.write('  pk: %d\n' % i)
+            f.write('  fields:\n')
+            f.write('    nama_po: %s\n' % csv_content[1])
+            f.write('    plat_no: %s\n' % csv_content[2])
+            f.write('    jenis_trayek: %s\n' % csv_content[3])
+            f.write('    jumlah_kursi: %s\n' % csv_content[4])
+            f.write('    asal_tujuan_trayek: %s\n' % csv_content[5])
+        f.close()
+
+from_csv_to_yaml('Data Bus PMLD - Sheet1.csv')
